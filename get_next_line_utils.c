53e2409588d6271ab25f71148007a94b79757e10
CassTirio22 <71003369+CassTirio22@users.noreply.github.com>
2020-12-29 16:16:21 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -31,10 +31,7 @@ void	*ft_calloc(int size, int count)
 
 	if (!(res = (char *)malloc(sizeof(void) * (size * count))))
 		return (NULL);
-	i = -1;
-	while (++i < size * count - 1)
-		;
-	res[i] = 0;
+	res[size * count - 1] = 0;
 	return ((void *)res);
 }
 
